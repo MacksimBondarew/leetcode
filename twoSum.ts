b9a5dcf652ee21008c38b2e32b102fcf2740b935
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-02-10 23:04:13 +0100

    add: first my solve problems on LeetCode

---
+++ twoSum.ts
@@ -0,0 +1,15 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+
+
+console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3,3], 6));
+console.log(twoSum([4,8, 4, 3], 8));
